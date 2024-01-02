c06c8be649a33a4a4712a2b574c822006b52d3a7
Ishaan-Goel <139787489+Ishaan-Goel@users.noreply.github.com>
2024-01-01 19:07:54 -0800

    An app that allows messaging between two apps

--- IMessages/AppDelegate.swift
+++ IMessages/AppDelegate.swift
@@ -1,19 +1,25 @@
 //
 //  AppDelegate.swift
 //  IMessages
-//
-//  Created by Ishaan Goel on 12/29/23.
-//
+//  Created by Ishaan Goel on 12/25/2023.
 
 import UIKit
+import Firebase
+import IQKeyboardManagerSwift
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
+        print(db)
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
         return true
     }
 
---
+++ IMessages/Message.swift
@@ -0,0 +1,11 @@
+//
+//  Message.swift
+//  IMessages
+//  Created by Ishaan Goel on 12/25/2023.
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
